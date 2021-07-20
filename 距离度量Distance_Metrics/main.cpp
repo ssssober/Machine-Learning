ef500e4540f6ed4507a3534ddb91ce046a6040f8
Self-Discipline <34881141+TheDetial@users.noreply.github.com>
2021-07-20 18:25:07 +0800

    hamming/edit/Jaccard

--- 距离度量Distance_Metrics/main.cpp
+++ 距离度量Distance_Metrics/main.cpp
@@ -1,15 +1,21 @@
 #include "dist.h"
 
+
+
 int main()
 {
 	DISTANCE dic;
-	dic.Init();
-	dic.CalEuclideanDis();
-	dic.DisPlay();
-	dic.CalManhattanDis();
-	dic.DisPlay();
-	dic.CalChebyshevDis();
-	dic.DisPlay();
+	//dic.Init();
+	//dic.CalEuclideanDis();//欧氏距离测试
+	//dic.DisPlay();
+	//dic.CalManhattanDis();//曼哈顿距离测试
+	//dic.DisPlay();
+	//dic.CalChebyshevDis();//切比雪夫距离测试
+	//dic.DisPlay();
+
+	//cout << dic.CalEditDis("intention", "execution")<< endl;  //编辑距离测试 结果为5
+
+	//dic.CalJaccardSimilarity();//杰卡德相似系数和距离测试
 	system("pause");
 	return 0;
 }
